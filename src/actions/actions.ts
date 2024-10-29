0c124b075220cc5dd536fb1c2408700eac3b0e63
Maria Starostina <maria.starostina.work@gmail.com>
2024-10-30 02:34:25 +0300

    Polish "Create Post" page

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,7 +1,7 @@
 'use server'
 export async function createPost(formData: FormData){
     const title = formData.get("title") as string;
-    console.log(title);
+    const body = formData.get("body");
 
     // await prisma.post.create({ //-update database: adding, updateing, deleting(data mutations))
     //     data:{
--- src/app/create-post/page.tsx
+++ src/app/create-post/page.tsx
@@ -8,18 +8,26 @@ export default async function page() {
       <h1 className="text-4xl md:text-5xl font-bold mb-5"> Create Post</h1>
       <form 
         action={createPost}
-        className="h-10 space-x-2 mt-10">
+        className="flex flex-col max-w-[500px] mx-auto gap-2 my-10">
         <input 
             type="text"
             name="title"
             placeholder="Title for new post"
-            className="border rounded px-3 h-full"
+            className="border rounded px-3 h-10"
             required
             />
-        <button className="h-full bg-slate-600 px-5 rounded text-white">Submit</button>
+            <textarea 
+              name="body"
+              placeholder="Body content for new post"
+              className="border rounded px-3 py-2"
+              rows={6}
+              required
+              >
+            </textarea>
+        <button className="h-10 bg-slate-600 px-5 rounded text-white">Submit</button>
       </form>
 
-      <LogoutLink>
+      <LogoutLink className="bg-slate-100 px-3 py-2 rounded">
         Log out
       </LogoutLink>
     </main>
