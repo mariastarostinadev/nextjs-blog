202ebe6642f05843db35e5fea89a226fbc027fd2
Maria Starostina <maria.starostina.work@gmail.com>
2024-10-30 03:49:50 +0300

    Setup Prisma and SQLite

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,11 +1,15 @@
 'use server'
+
+import prisma from "@/lib/db";
+
 export async function createPost(formData: FormData){
     const title = formData.get("title") as string;
-    const body = formData.get("body");
+    const body = formData.get("body") as string;
 
-    // await prisma.post.create({ //-update database: adding, updateing, deleting(data mutations))
-    //     data:{
-    //         title,
-    //     },
-    // })
+    await prisma.post.create({ //-update database: adding, updateing, deleting(data mutations))
+        data:{
+            title,
+            body
+        },
+    })
 }
